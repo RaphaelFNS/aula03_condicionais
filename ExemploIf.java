c953a8db52a9173a0aa51f572e71035d787aec53
Raphael Nascimento <raphael.boy.fn@gmail.com>
2023-09-11 21:07:36 -0300

    aula03 final exemplo if

--- ExemploIf.java
+++ ExemploIf.java
@@ -1,6 +1,13 @@
 import javax.swing.JOptionPane;
 public class ExemploIf {
     static public void main(String [] args){
-        
+        double nota = Double.parseDouble(JOptionPane.showInputDialog("Digite a nota"));
+
+        if(nota >= 70){
+            JOptionPane.showMessageDialog(null, "Aprovado");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Reprovado");
+        }
     }   
 }
